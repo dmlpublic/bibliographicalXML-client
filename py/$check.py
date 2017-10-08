f66477be69fb2ed30f5a9f1ec15b871953cb575b
David <dml.public@sbglobal.net>
2017-10-08 12:04:17 -0700

    Move attributes to an attribute group
    This allows XML and XSL to treat elements and attributes the same in terms
    of namespaces.

--- py/$check.py
+++ py/$check.py
@@ -3,8 +3,6 @@ sys.path.insert(0, '..\\bibliographicalXML\\scripts')
 
 import checkCollation
 import utilities
-from checkBibl import checkBibl
-
 
 # files = ['..\\xml\\minimal.xml']
 files = utilities.getXMLfiles()
